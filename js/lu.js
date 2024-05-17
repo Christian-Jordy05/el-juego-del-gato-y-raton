b4b101cfd682eb43791b118660c20bd153fcd6cc
Christian-Jordy05 <yordymar224@gmail.com>
2024-05-17 15:43:57 -0600

    puse las funciones al juego para que se ponga las X y O

--- js/lu.js
+++ js/lu.js
@@ -7,167 +7,241 @@ let cuad6 = document.getElementById("cuadro6");
 let cuad7 = document.getElementById("cuadro7");
 let cuad8 = document.getElementById("cuadro8");
 let cuad9 = document.getElementById("cuadro9");
+/* todos los codigos de arriba llama cada cuadritos*/
 
 
-let gato = true
 
-clave1 = true;
+let matriz = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+]
 
-function cuadro1(){
-    if (clave1 === true) {
+// matriz2(matriz)
+// function matriz2() {
+
+//     matriz[2][0] = "X"
+//      console.log(matriz);
+// }
+
+
+
+let gato = true;
+
+turno1 = true;
+
+function cuadro1() {
+    if (turno1 === true) {
         if (gato === true) {
             cuad1.innerHTML = 'X'
             gato = false;
+            matriz[0][0] = "X"
+            console.log(matriz);
         } else {
             cuad1.innerHTML = 'O'
             gato = true;
+            matriz[0][0] = "O"
+            console.log(matriz);
         }
-        clave1 = false;
+        turno1 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
+
+}
+
+matriz2(cuadro1)
+function matriz2() {
+
+
 }
 
 
 
-clave2 = true;
 
-function cuadro2(){
-    if (clave2 === true) {
+turno2 = true;
+
+function cuadro2() {
+    if (turno2 === true) {
         if (gato === true) {
             cuad2.innerHTML = 'X'
             gato = false;
+            matriz[0][1] = "X"
+            console.log(matriz);
         } else {
             cuad2.innerHTML = 'O'
             gato = true;
+            matriz[0][1] = "O"
+            console.log(matriz);
         }
-        clave2 = false;
+        turno2 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 
 
 
-clave3 = true;
+turno3 = true;
 
-function cuadro3(){
-    if (clave3 === true) {
+function cuadro3() {
+    if (turno3 === true) {
         if (gato === true) {
             cuad3.innerHTML = 'X'
             gato = false;
+            matriz[0][2] = "X"
+            console.log(matriz);
         } else {
             cuad3.innerHTML = 'O'
             gato = true;
+            matriz[0][2] = "O"
+            console.log(matriz);
         }
-        clave3 = false;
+        turno3 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 
 
 /*--------------------------------------------------*/
-clave4 = true;
+turno4 = true;
 
-function cuadro4(){
-    if (clave4 === true) {
+function cuadro4() {
+    if (turno4 === true) {
         if (gato === true) {
             cuad4.innerHTML = 'X'
             gato = false;
+            matriz[1][0] = "X"
+            console.log(matriz);
         } else {
             cuad4.innerHTML = 'O'
             gato = true;
+            matriz[1][0] = "O"
+            console.log(matriz);
         }
-        clave4 = false;
+        turno4 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 
 /*--------------------------------------------------*/
-clave5 = true;
+turno5 = true;
 
-function cuadro5(){
-    if (clave5 === true) {
+function cuadro5() {
+    if (turno5 === true) {
         if (gato === true) {
             cuad5.innerHTML = 'X'
             gato = false;
+            matriz[1][1] = "X"
+            console.log(matriz);
         } else {
             cuad5.innerHTML = 'O'
             gato = true;
+            matriz[1][1] = "O"
+            console.log(matriz);
         }
-        clave5 = false;
+        turno5 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 /*--------------------------------------------------*/
-clave6 = true;
+turno6 = true;
 
-function cuadro6(){
-    if (clave6 === true) {
+function cuadro6() {
+    if (turno6 === true) {
         if (gato === true) {
             cuad6.innerHTML = 'X'
             gato = false;
+              matriz[1][2] = "X"
+            console.log(matriz);
         } else {
             cuad6.innerHTML = 'O'
             gato = true;
+            matriz[1][2] = "O"
+            console.log(matriz);
         }
-        clave6 = false;
+        turno6 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 /*--------------------------------------------------*/
-clave7 = true;
+turno7 = true;
 
-function cuadro7(){
-    if (clave7 === true) {
+
+function cuadro7() {
+    if (turno7 === true) {
         if (gato === true) {
             cuad7.innerHTML = 'X'
             gato = false;
+            matriz[2][0] = "X"
+            console.log(matriz);
         } else {
             cuad7.innerHTML = 'O'
             gato = true;
+            matriz[2][0] = "O"
+            console.log(matriz);
         }
-        clave7 = false;
+        turno7 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 /*--------------------------------------------------*/
-clave8 = true;
+turno8 = true;
 
-function cuadro8(){
-    if (clave8 === true) {
+function cuadro8() {
+    if (turno8 === true) {
         if (gato === true) {
             cuad8.innerHTML = 'X'
             gato = false;
+            matriz[2][1] = "X"
+            console.log(matriz);
         } else {
             cuad8.innerHTML = 'O'
             gato = true;
+            matriz[2][1] = "O"
+            console.log(matriz);
         }
-        clave8 = false;
+        turno8 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
     }
 }
 /*--------------------------------------------------*/
-clave9 = true;
+turno9 = true;
 
-function cuadro9(){
-    if (clave9 === true) {
+function cuadro9() {
+    if (turno9 === true) {
         if (gato === true) {
             cuad9.innerHTML = 'X'
             gato = false;
+            matriz[2][2] = "X"
+            console.log(matriz);
         } else {
             cuad9.innerHTML = 'O'
             gato = true;
+            matriz[2][2] = "O"
+            console.log(matriz);
         }
-        clave9 = false;
+        turno9 = false;
     } else {
-        console.log(':(');
+        alert("este campo esta ocupado");
+    }
+}
+
+
+function primeraFIla(matriz) {
+    let contaX = 0;
+    let contaO = 0;
+
+    for (let index = 0; index < matriz.length; index++) {
+        if (matriz[0][index] = "X") {
+            contaX++;
+        }
     }
 }
 
@@ -177,8 +251,7 @@ function cuadro9(){
 
 
 
-
-// console.log(matriz[0][0]); /* los numeros van hacer las coordenadas de los cuadro*/
+// console.log(matriz); /* los numeros van hacer las coordenadas de los cuadro*/
 
 // matriz[0][0] = "X"
 
