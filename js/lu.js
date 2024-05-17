d664473d71b95c1bc2f1a847756d7b54b9c2c437
Christian-Jordy05 <yordymar224@gmail.com>
2024-05-16 21:45:25 -0600

    puse un div y en ese div puse otros div para los cuadros y tambien puse la funcionalidad del juego

---
+++ js/lu.js
@@ -0,0 +1,185 @@
+let cuad1 = document.getElementById("cuadro1");
+let cuad2 = document.getElementById("cuadro2");
+let cuad3 = document.getElementById("cuadro3");
+let cuad4 = document.getElementById("cuadro4");
+let cuad5 = document.getElementById("cuadro5");
+let cuad6 = document.getElementById("cuadro6");
+let cuad7 = document.getElementById("cuadro7");
+let cuad8 = document.getElementById("cuadro8");
+let cuad9 = document.getElementById("cuadro9");
+
+
+let gato = true
+
+clave1 = true;
+
+function cuadro1(){
+    if (clave1 === true) {
+        if (gato === true) {
+            cuad1.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad1.innerHTML = 'O'
+            gato = true;
+        }
+        clave1 = false;
+    } else {
+        console.log(':(');
+    }
+}
+
+
+
+clave2 = true;
+
+function cuadro2(){
+    if (clave2 === true) {
+        if (gato === true) {
+            cuad2.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad2.innerHTML = 'O'
+            gato = true;
+        }
+        clave2 = false;
+    } else {
+        console.log(':(');
+    }
+}
+
+
+
+clave3 = true;
+
+function cuadro3(){
+    if (clave3 === true) {
+        if (gato === true) {
+            cuad3.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad3.innerHTML = 'O'
+            gato = true;
+        }
+        clave3 = false;
+    } else {
+        console.log(':(');
+    }
+}
+
+
+/*--------------------------------------------------*/
+clave4 = true;
+
+function cuadro4(){
+    if (clave4 === true) {
+        if (gato === true) {
+            cuad4.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad4.innerHTML = 'O'
+            gato = true;
+        }
+        clave4 = false;
+    } else {
+        console.log(':(');
+    }
+}
+
+/*--------------------------------------------------*/
+clave5 = true;
+
+function cuadro5(){
+    if (clave5 === true) {
+        if (gato === true) {
+            cuad5.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad5.innerHTML = 'O'
+            gato = true;
+        }
+        clave5 = false;
+    } else {
+        console.log(':(');
+    }
+}
+/*--------------------------------------------------*/
+clave6 = true;
+
+function cuadro6(){
+    if (clave6 === true) {
+        if (gato === true) {
+            cuad6.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad6.innerHTML = 'O'
+            gato = true;
+        }
+        clave6 = false;
+    } else {
+        console.log(':(');
+    }
+}
+/*--------------------------------------------------*/
+clave7 = true;
+
+function cuadro7(){
+    if (clave7 === true) {
+        if (gato === true) {
+            cuad7.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad7.innerHTML = 'O'
+            gato = true;
+        }
+        clave7 = false;
+    } else {
+        console.log(':(');
+    }
+}
+/*--------------------------------------------------*/
+clave8 = true;
+
+function cuadro8(){
+    if (clave8 === true) {
+        if (gato === true) {
+            cuad8.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad8.innerHTML = 'O'
+            gato = true;
+        }
+        clave8 = false;
+    } else {
+        console.log(':(');
+    }
+}
+/*--------------------------------------------------*/
+clave9 = true;
+
+function cuadro9(){
+    if (clave9 === true) {
+        if (gato === true) {
+            cuad9.innerHTML = 'X'
+            gato = false;
+        } else {
+            cuad9.innerHTML = 'O'
+            gato = true;
+        }
+        clave9 = false;
+    } else {
+        console.log(':(');
+    }
+}
+
+
+
+
+
+
+
+
+// console.log(matriz[0][0]); /* los numeros van hacer las coordenadas de los cuadro*/
+
+// matriz[0][0] = "X"
+
+// console.log(matriz);
